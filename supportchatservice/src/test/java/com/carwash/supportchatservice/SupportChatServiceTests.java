ae39b026758b548a70e37b31861c5d9819633b79
Premchand <chandupremgunda@gmail.com>
2026-04-19 09:27:10 +0530

    Fix: exclude mail auto-config from supportchatservice test

--- supportchatservice/src/test/java/com/carwash/supportchatservice/SupportChatServiceTests.java
+++ supportchatservice/src/test/java/com/carwash/supportchatservice/SupportChatServiceTests.java
@@ -3,7 +3,7 @@ package com.carwash.supportchatservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.mail.MailSenderAutoConfiguration")
 class SupportChatServiceTests {
 
 	@Test
